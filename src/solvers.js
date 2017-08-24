789575005d85fcf8187f8c9b03584388af336cae
Alex Ko <alexkosj@gmail.com>
2017-08-24 11:30:55 -0700

    Doc: implement nRooks

--- src/solvers.js
+++ src/solvers.js
@@ -16,7 +16,26 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+
+  var board = new Board({'n': n});
+  board.get(0)[0] = 1;
+  var size = board.get('n');
+  var solution = [];
+
+  for (var i = 0; i < size; i++) {
+    for (var j = 0; j < size; j++) {
+      board.get(i)[j] = 1;
+      if (board.hasAnyRooksConflicts()) {
+        board.get(i)[j] = 0;
+      }
+    }
+  }
+
+  for (var k = 0; k < size; k++) {
+    solution.push(board.get(k));
+  }
+  
+
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
@@ -24,11 +43,19 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = factorial(n); //fixme
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
+
+window.factorial = function(n) {
+  if (n === 0) {
+    return 1;
+  }
+
+  return n * factorial(n-1);
+}
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
